87d2c9c02c184847f0645ea8f56a244a8228700c
patriot1488 <borisgodynof@example.com>
2021-04-17 10:05:48 +0300

    2

---
+++ LAB_2_1/LAB_2_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LAB_2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tmp;
+            tmp = Console.ReadLine();
+            Int64 a = 0, b = 0;
+            for(int i = 0; i < tmp.Length; i++)
+            {
+                if(tmp[i] == ' ')
+                {
+                    a = Convert.ToInt64(tmp.Substring(0,i));
+                    b = Convert.ToInt64(tmp.Substring(i));
+                }
+            }
+
+            int ans = 0;
+            for(Int64 i = a; i <= b; i++)
+            {
+                Int64 T = i;
+                while (T % 2 == 0) {
+                    T /= 2;
+                    ans++;
+                }
+            }
+
+            Console.WriteLine(ans);
+
+        }
+    }
+}
